600d4af05d29419d6bf473d0159cb6fa2fea5124
sya-ri <git@s7a.dev>
2023-12-23 16:18:47 +0900

    fix(bungee): Save default config

--- bungee/src/main/java/dev/s7a/mseds/bungee/Config.java
+++ bungee/src/main/java/dev/s7a/mseds/bungee/Config.java
@@ -9,13 +9,35 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Config implements MinecraftServerExceptionDiscordSenderConfig {
     private final Configuration configuration;
 
     public Config(@NotNull Plugin plugin) throws IOException {
         File file = new File(plugin.getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            // saveDefaultConfig
+            if (!plugin.getDataFolder().exists() && !plugin.getDataFolder().mkdirs()) {
+                throw new IOException();
+            }
+            FileOutputStream outputStream = new FileOutputStream(file);
+            URL url = this.getClass().getClassLoader().getResource("config.yml");
+            URLConnection connection = url.openConnection();
+            connection.setUseCaches(false);
+            InputStream inputStream = connection.getInputStream();
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        }
         configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
     }
 
